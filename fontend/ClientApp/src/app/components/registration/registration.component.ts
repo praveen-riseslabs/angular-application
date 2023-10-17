b52011f62fc2eac09b38469a20e37afc429db0b1
laxminarayanariseslabs <laxminarayana.sirimalla@riseslabs.com>
2023-10-17 17:17:33 +0530

    validations for registration added

--- fontend/ClientApp/src/app/components/registration/registration.component.ts
+++ fontend/ClientApp/src/app/components/registration/registration.component.ts
@@ -10,6 +10,8 @@ export class RegistrationComponent implements OnInit {
 
   userdata:Userdata = new Userdata();
   userlist:Userdata[] = [];
+  confirmPassword : string = '';
+  passwordError:string = '';
   constructor(private userdataservice : UserdataserviceService) { }
 
   ngOnInit() {
@@ -34,5 +36,33 @@ saveUserData() {
     // localStorage.setItem('registerUsers', JSON.stringify(this.userlist))
   })
 }
+
+validateFields(value: string): boolean {
+  
+  if(this.userdata.Firstname !== undefined){
+    const regex = /^[A-Za-z]+$/; 
+  return regex.test(value);
+  }
+  else if(this.userdata.Lastname !== undefined){
+    const regex = /^[A-Za-z]+$/; 
+    return regex.test(value);
+  }
+  else if(this.userdata.Email !== undefined){
+  const regex = /^[a-zA-Z][a-zA-Z0-9._-]*@[a-zA-Z0-9.-]+\.[a-zA-Z]{3}$/; 
+  return regex.test(value);
+  }
+  else if(this.userdata.Password !==  undefined){
+    const regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{8,16}$/; 
+    return regex.test(value);
+  }
+}
+validateConfirmPassword(){
+  this.passwordError = this.userdata.Password !== this.confirmPassword ? 'Passwords do not match' : '';
+}
+isFormValid(): boolean {
+  return this.userdata && this.confirmPassword && !this.passwordError;
+}
+
+
 
 }
