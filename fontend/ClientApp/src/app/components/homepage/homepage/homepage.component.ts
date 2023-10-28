8efc57793c4137c4ae586c95497bf503d84c2808
laxminarayanariseslabs <laxminarayana.sirimalla@riseslabs.com>
2023-10-28 13:46:44 +0530

    Profile icon with dropdown

--- fontend/ClientApp/src/app/components/homepage/homepage/homepage.component.ts
+++ fontend/ClientApp/src/app/components/homepage/homepage/homepage.component.ts
@@ -1,29 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { Renderer2, ElementRef, ViewChild } from '@angular/core';
+import { Renderer2, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import $ from 'jquery';
+declare var M: any; // Declare Materialize as any
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent implements OnInit {
+
+
+export class HomepageComponent implements OnInit, AfterViewInit {
   @ViewChild('userProfile') userProfile: ElementRef;
   @ViewChild('dropdown1') dropdown1: ElementRef;
 
-  constructor(private renderer: Renderer2) { }
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
 
   ngOnInit() {
+
   }
   
-  toggleDropdown() {
-    debugger
-    const dropdown = this.dropdown1.nativeElement;
-
-    if (dropdown.style.display === 'block') {
-      this.renderer.setStyle(dropdown, 'display', 'none');
-    } else {
-      this.renderer.setStyle(dropdown, 'display', 'block');
-    }
+ 
+  ngAfterViewInit() {
+    const dropdownTrigger = this.el.nativeElement.querySelector('.dropdown-trigger');
+    M.Dropdown.init(dropdownTrigger, {
+      constrainWidth: false 
+    });
   }
 
+  
+  
+  
 }
+
+
+
+
